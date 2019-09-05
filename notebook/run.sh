b1cd37a027e6e477489784770ff1f60c14791838
Orhan Hirsch <orhanhenrik@gmail.com>
2019-09-05 11:08:18 +0200

    Move notebook folder

--- notebook/run.sh
+++
@@ -1,2 +0,0 @@
-# docker run -it -p 8888:8888 -v "$(pwd)/work:/home/jovyan/work" jupyter/datascience-notebook
-docker run -it -p 8888:8888 -v "$(pwd)/work:/home/jovyan/work" pm4py-notebook
---
+++ notebooks/run.sh
@@ -0,0 +1,1 @@
+docker run -it -p 8888:8888 -v "$(pwd)/work:/home/jovyan/work" ntnu-process-mining-notebook
