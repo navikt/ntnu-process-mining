075e529f7fe7fca32e295f6e0238f3c03255eba4
Lars Gjelstad <lars@dhcp-10-22-6-23.wlan.ntnu.no>
2019-09-06 12:46:49 +0200

    #2 added black for python lint and circleci config file to allow for circleCI integration

--- ntnu_process_mining/__init__.py
+++ ntnu_process_mining/__init__.py
@@ -2,10 +2,13 @@ from ._version import version_info, __version__
 
 from .example import *
 
+
 def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'static',
-        'dest': 'ntnu-process-mining',
-        'require': 'ntnu-process-mining/extension'
-    }]
+    return [
+        {
+            "section": "notebook",
+            "src": "static",
+            "dest": "ntnu-process-mining",
+            "require": "ntnu-process-mining/extension",
+        }
+    ]
