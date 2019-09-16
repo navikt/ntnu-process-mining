bffd75281b4bc4b8086e3614204d41601caa3b29
Lars Gjelstad <lars@dhcp-10-24-2-23.wlan.ntnu.no>
2019-09-16 11:45:06 +0200

    #2 added eslint ts compatability and prettier

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -4,18 +4,19 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": ["eslint:recommended", "prettier"],
-    "plugins": ["prettier"],
+    "parser": ["@typescript-eslint/parser"],                    // Specify ESlint parser
+    "extends": ["plugin:@typescript-eslint/recommended",        // Use reccommended rules from TypeScript-eslint
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"],                             // Use reccommended rues from prettier and display prettier errors as ESlint errors. Must be last config in array
+    "plugins": ["prettier"],                                    // Include plugin prettier
     "rules": {
-        "prettier/prettier": ["error"]
     },
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018
+        "ecmaVersion": 2018,
+        "sourceType": 'module',
     },
-    "rules": {
-    }
 };
