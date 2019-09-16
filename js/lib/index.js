e2a28a5830836c551b73ae443d6d03f0f3f27734
Orhan Hirsch <orhanhenrik@gmail.com>
2019-09-16 11:00:12 +0200

    remove lib folder

--- js/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./ProcessMap"));
-__export(require("./ProcessMapModel"));
-exports.version = require('../package.json').version;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxrQ0FBNkI7QUFDN0IsdUNBQWtDO0FBRXZCLFFBQUEsT0FBTyxHQUFHLE9BQU8sQ0FBQyxpQkFBaUIsQ0FBQyxDQUFDLE9BQU8sQ0FBQyJ9
