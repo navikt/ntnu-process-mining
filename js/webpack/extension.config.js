89380e6b2ed31991c4df6e01947433d2e947c85b
Orhan Hirsch <orhanhenrik@gmail.com>
2019-09-17 12:19:52 +0200

    Improve webpack config and enable webpack-dev-server for faster local development. Add some docs on how to run module in development.

---
+++ js/webpack/extension.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+module.exports = {
+  // Notebook extension
+  //
+  // This bundle only contains the part of the JavaScript that is run on
+  // load of the notebook. This section generally only performs
+  // some configuration for requirejs, and provides the legacy
+  // "load_ipython_extension" function which is required for any notebook
+  // extension.
+  //
+  entry: './src/extension.ts',
+  output: {
+    filename: 'extension.js',
+    path: path.resolve(__dirname, '..', '..', 'ntnu_process_mining', 'static'),
+    libraryTarget: 'amd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+};
