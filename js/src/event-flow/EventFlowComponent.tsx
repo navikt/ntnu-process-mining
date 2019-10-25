012bb9be9548c09cdb1b77bb4cb96bbe206b3595
Orhan Hirsch <orhanhenrik@gmail.com>
2019-10-25 10:34:26 +0200

    Hacky setState to set default scale to event sequence. Also add min event count = 2

--- js/src/event-flow/EventFlowComponent.tsx
+++ js/src/event-flow/EventFlowComponent.tsx
@@ -1,9 +1,15 @@
 import { App } from '@data-ui/event-flow';
 import { withScreenSize } from '@vx/responsive';
-import React from 'react';
+import React, { useCallback } from 'react';
 
-const ResponsiveVis = withScreenSize(({ screenWidth, ...rest }) => (
-  <App width={screenWidth * 0.7} height={500} {...rest} />
+const ResponsiveVis = withScreenSize(({ screenWidth, theRef, ...rest }) => (
+  <App
+    width={screenWidth * 0.7}
+    height={500}
+    ref={theRef}
+    initialMinEventCount={2}
+    {...rest}
+  />
 ));
 
 export interface IEvent {
@@ -13,10 +19,18 @@ export interface IEvent {
 }
 
 const EventFlowComponent: React.FC<{ events: IEvent[] }> = ({ events }) => {
+  const ref = useCallback(node => {
+    if (node !== null) {
+      node.setState({
+        xScaleType: 'EVENT_SEQUENCE_SCALE'
+      });
+    }
+  }, []);
+
   return (
     <div className={'content'}>
       <h1>Event-flow</h1>
-      <ResponsiveVis data={events} />
+      <ResponsiveVis theRef={ref} data={events} />
     </div>
   );
 };
