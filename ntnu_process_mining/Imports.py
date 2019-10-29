0a6b90ff7772c5fba1775e5e7fe1ead36c9c8d86
henriner <henriner@stud.ntnu.no>
2019-10-29 11:40:32 +0100

    #16 Removed unwanted components with css

--- ntnu_process_mining/Imports.py
+++ ntnu_process_mining/Imports.py
@@ -5,9 +5,13 @@ from pm4py.objects.log.adapters.pandas import csv_import_adapter
 from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
 from pm4py import util as pmutil
 from pandas import read_csv
+from IPython.display import HTML
+
+
 
 
 def Event(file_in):
+    
     file = read_csv(os.path.join("files", "input_data", file_in))
     
     log = csv_import_adapter.convert_timestamp_columns_in_df(file, timest_columns=['timestamp'])
@@ -22,4 +26,7 @@ def Event(file_in):
     
     ef = EventFlow()
     ef.value = events
-    return ef
+    return  ef
+
+def Style():
+    return HTML('<style>.Select-multi-value-wrapper{height:2ch;} .Select-multi-value-wrapper input{display:none;} .Select-input{display:none;}</style>')
