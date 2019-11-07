7cefd59ab07e78bd95c35665ea1f3931ae39ba61
Morten Smebye <mortensmeb@gmail.com>
2019-11-07 01:44:06 +0100

    Add trace building interactivity

--- js/src/TraceComparisonView.ts
+++ js/src/TraceComparisonView.ts
@@ -1,32 +1,71 @@
 import { DOMWidgetView } from '@jupyter-widgets/base';
+import Sortable from 'sortablejs';
 
 export class TraceComparisonView extends DOMWidgetView {
   public render() {
-    /* let result = this.model.get('result'); */
-    let activities = this.model.get('activities');
-    // let trace = this.model.get('trace');
+    const activities = this.model.get('activities');
 
-    let selectionBox = document.createElement('select');
-    for (var i = 0; i < activities.length; i++) {
-      let option = document.createElement('option');
-      option.value = activities[i];
-      option.text = activities[i];
-      selectionBox.appendChild(option);
+    const activitiesList = document.createElement('ul');
+
+    // Create the list of all available activities
+    for (const activity of activities) {
+      const option = document.createElement('li');
+      option.textContent = activity;
+      activitiesList.appendChild(option);
     }
+    Sortable.create(activitiesList, {
+      group: {
+        name: 'same',
+        pull: 'clone',
+        put: false
+      },
+      sort: false,
+      animation: 150
+    });
 
-    let currentTrace = document.createElement('textarea');
+    // Create the list of the built trace
+    const traceList = document.createElement('ul');
 
-    function selectionOnClick() {
-      const newTrace = [...this.model.get('trace'), selectionBox.value];
-      currentTrace.value = newTrace.toString();
+    Sortable.create(traceList, {
+      group: {
+        name: 'same'
+      },
+      animation: 150,
+      onAdd: function(event) {
+        let option = event.item;
+        let button = document.createElement('button');
+        let clicked = function(item) {
+          item.parentNode.removeChild(item);
+          this.el.dispatchEvent(new Event('change'));
+        };
+        button.textContent = 'X';
+        button.onclick = clicked.bind(this, event.item);
+        option.appendChild(button);
+        event.item.replaceWith(option);
+      },
+      onSort: function(event) {
+        this.el.dispatchEvent(new Event('change', { bubbles: true }));
+      }
+    });
+
+    let changed = function() {
+      // Convert HTML li elements to javascript array
+      let selectedActivities = traceList.getElementsByTagName('li');
+      let newTrace = [];
+      for (const activity of selectedActivities) {
+        // Remove button tag
+        let regex = /<button.*?<\/button>/gi;
+        let result = activity.innerHTML.replace(regex, '');
+        newTrace.push(result);
+      }
+
       this.model.set('trace', newTrace);
       this.model.save_changes();
-    }
+    };
 
-    selectionBox.addEventListener('change', selectionOnClick.bind(this));
-
-    this.el.appendChild(selectionBox);
-    this.el.appendChild(currentTrace);
+    traceList.addEventListener('change', changed.bind(this));
+    this.el.appendChild(activitiesList);
+    this.el.appendChild(traceList);
 
     let table = document.createElement('tbody');
 
