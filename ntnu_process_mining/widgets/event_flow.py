4d8c1a02413415c3a43d337be6ceb4373286de46
Orhan Hirsch <orhanhenrik@gmail.com>
2019-11-07 11:35:31 +0100

    Add field name options to event flow

--- ntnu_process_mining/widgets/event_flow.py
+++ ntnu_process_mining/widgets/event_flow.py
@@ -14,15 +14,21 @@ class EventFlow(widgets.Widget):
     _model_module_version = Unicode("^0.1.0").tag(sync=True)
     value = List(Dict, default_value=[]).tag(sync=True)
 
-    def __init__(self, df):
+    def __init__(
+            self,
+            df,
+            timestamp_field='timestamp',
+            case_id_field='case_id',
+            activity_field='activity'):
+
         super(EventFlow, self).__init__()
-        self.df = csv_import_adapter.convert_timestamp_columns_in_df(df, timest_columns=['timestamp'])
+        self.df = csv_import_adapter.convert_timestamp_columns_in_df(df, timest_columns=[timestamp_field])
         events = []
         for index, row in self.df.iterrows():
             events.append({
-                "ENTITY_ID": row['case_id'],
-                "TS": row['timestamp'],
-                "EVENT_NAME": row['activity']
+                "ENTITY_ID": row[case_id_field],
+                "TS": row[timestamp_field],
+                "EVENT_NAME": row[activity_field]
             })
         self.value = events
 
